ec50d9dd5f48e51719ff2a24df03e246587212fd
pyrromanis <gpyrros@proton.me>
2023-06-06 15:45:38 +0000

    runtime-rs: firecracker hypervisor backend
    This is an initial, WiP patch to support firecracker in runtime-rs.
    Currently, as device hotplug is not supported, nerdctl/docker do
    not work as the network setup is done after the sandbox creation
    (see kata-containers/kata-containers#6384).
    
    Assumes FC binaries are in `$INSTALL_PREFIX/bin`
    
    Fixes: #5268
    
    Signed-off-by: George Pyrros <gpyrros@nubificus.co.uk>
    Signed-off-by: Anastassios Nanos <ananos@nubificus.co.uk>

---
+++ src/runtime-rs/crates/hypervisor/src/firecracker/inner_hypervisor.rs
@@ -0,0 +1,148 @@
+use crate::firecracker::{
+    utils::{get_sandbox_path, get_vsock_path},
+    FcInner,
+};
+use crate::{VcpuThreadIds, VmmState, HYPERVISOR_FIRECRACKER};
+use anyhow::{anyhow, Context, Result};
+use kata_types::capabilities::Capabilities;
+use shim_interface::KATA_PATH;
+use std::collections::HashSet;
+use std::iter::FromIterator;
+use tokio::fs;
+
+const HYBRID_VSOCK_SCHEME: &str = "hvsock";
+
+impl FcInner {
+    pub(crate) async fn prepare_vm(&mut self, id: &str, _netns: Option<String>) -> Result<()> {
+        info!(sl!(), "Preparing Firecracker");
+
+        let sb_path = get_sandbox_path(id)?;
+        self.id = id.to_string();
+
+        if !self.config.jailer_path.is_empty() {
+            self.jailed = true;
+            self.jailer_root = KATA_PATH.to_string();
+            self.vm_path = [KATA_PATH, HYPERVISOR_FIRECRACKER, id].join("/");
+            let _ = self.remount_jailer_with_exec().await;
+        } else {
+            let _ = fs::create_dir_all(&sb_path)
+                .await
+                .context(format!("failed to create directory {:?}", &sb_path));
+            self.vm_path = sb_path;
+        }
+
+        self.prepare_api_socket(id).await?;
+        self.netns = _netns.clone();
+        self.prepare_vmm(self.netns.clone()).await?;
+        self.state = VmmState::VmmServerReady;
+        self.prepare_vmm_resources(id).await?;
+        self.prepare_hvsock(id).await?;
+        Ok(())
+    }
+
+    pub(crate) async fn start_vm(&mut self, _timeout: i32) -> Result<()> {
+        info!(sl!(), "Starting Firecracker sandbox");
+        self.instance_start().await?;
+        self.state = VmmState::VmRunning;
+        Ok(())
+    }
+
+    pub(crate) async fn stop_vm(&mut self) -> Result<()> {
+        info!(sl!(), "Stopping Firecracker sandbox");
+        if self.state != VmmState::VmRunning {
+            info!(sl!(), "Cannot stop Firecracker as it is not running!");
+        } else if let Some(pid_to_kill) = &self.pid {
+            let pid = ::nix::unistd::Pid::from_raw(*pid_to_kill as i32);
+            if let Err(err) = ::nix::sys::signal::kill(pid, nix::sys::signal::SIGKILL) {
+                if err != ::nix::Error::ESRCH {
+                    info!(
+                        sl!(),
+                        "failed to kill Firecracker with pid {} {:?}", pid, err
+                    );
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub(crate) fn pause_vm(&self) -> Result<()> {
+        info!(sl!(), "Pausing Firecracker");
+        Ok(())
+    }
+
+    pub(crate) async fn save_vm(&self) -> Result<()> {
+        info!(sl!(), "Saving Firecracker");
+        Ok(())
+    }
+    pub(crate) fn resume_vm(&self) -> Result<()> {
+        info!(sl!(), "Resuming Firecracker");
+        Ok(())
+    }
+
+    pub(crate) async fn get_agent_socket(&self) -> Result<String> {
+        info!(sl!(), "FcInner: Get agent socket");
+        let vsock_path = get_vsock_path(&self.id, self.jailed, false)?;
+        Ok(format!("{}://{}", HYBRID_VSOCK_SCHEME, vsock_path))
+    }
+
+    pub(crate) async fn disconnect(&mut self) {
+        info!(sl!(), "FcInner: Disconnect");
+    }
+    pub(crate) async fn get_thread_ids(&self) -> Result<VcpuThreadIds> {
+        info!(sl!(), "FcInner: Getthread ids");
+        Ok(VcpuThreadIds::default())
+    }
+
+    pub(crate) async fn get_pids(&self) -> Result<Vec<u32>> {
+        info!(sl!(), "FcInner: Get pids");
+        let mut pids = HashSet::new();
+        // get shim thread ids
+        pids.insert(self.pid.unwrap());
+
+        info!(sl!(), "get pids {:?}", pids);
+        Ok(Vec::from_iter(pids.into_iter()))
+    }
+
+    pub(crate) async fn get_vmm_master_tid(&self) -> Result<u32> {
+        info!(sl!(), "FcInner: get vmm master tid");
+        if let Some(pid) = self.pid {
+            Ok(pid)
+        } else {
+            Err(anyhow!("could not get vmm master tid"))
+        }
+    }
+    pub(crate) async fn get_ns_path(&self) -> Result<String> {
+        info!(sl!(), "FcInner: get ns path");
+        if let Some(pid) = self.pid {
+            let ns_path = format!("/proc/{}/ns", pid);
+            Ok(ns_path)
+        } else {
+            Err(anyhow!("could not get ns path"))
+        }
+    }
+
+    pub(crate) async fn cleanup(&self) -> Result<()> {
+        info!(sl!(), "FcInner: Cleanup");
+        let sb_path = get_sandbox_path(&self.id)?;
+        self.cleanup_resource(&sb_path);
+        Ok(())
+    }
+
+    pub(crate) async fn resize_vcpu(&self, old_vcpu: u32, new_vcpu: u32) -> Result<(u32, u32)> {
+        Ok((old_vcpu, new_vcpu))
+    }
+
+    pub(crate) async fn check(&self) -> Result<()> {
+        info!(sl!(), "FcInner: Check");
+        Ok(())
+    }
+
+    pub(crate) async fn get_jailer_root(&self) -> Result<String> {
+        info!(sl!(), "FcInner: Get jailer root");
+        Ok(self.jailer_root.clone())
+    }
+
+    pub(crate) async fn capabilities(&self) -> Result<Capabilities> {
+        Ok(self.capabilities.clone())
+    }
+}
---
+++ src/runtime-rs/crates/hypervisor/src/firecracker/mod.rs
@@ -0,0 +1,166 @@
+mod fc_api;
+mod inner;
+mod inner_device;
+mod inner_hypervisor;
+mod utils;
+
+use super::HypervisorState;
+use crate::{device::DeviceType, Hypervisor, HypervisorConfig, VcpuThreadIds};
+use anyhow::Context;
+use anyhow::Result;
+use async_trait::async_trait;
+use inner::FcInner;
+use kata_types::capabilities::Capabilities;
+use persist::sandbox_persist::Persist;
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
+#[derive(Debug)]
+pub struct Firecracker {
+    inner: Arc<RwLock<FcInner>>,
+}
+
+impl Default for Firecracker {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Firecracker {
+    pub fn new() -> Self {
+        Self {
+            inner: Arc::new(RwLock::new(FcInner::new())),
+        }
+    }
+
+    pub async fn set_hypervisor_config(&mut self, config: HypervisorConfig) {
+        let mut inner = self.inner.write().await;
+        inner.set_hypervisor_config(config)
+    }
+}
+
+#[async_trait]
+impl Hypervisor for Firecracker {
+    async fn prepare_vm(&self, id: &str, netns: Option<String>) -> Result<()> {
+        let mut inner = self.inner.write().await;
+        inner.prepare_vm(id, netns).await
+    }
+
+    async fn start_vm(&self, timeout: i32) -> Result<()> {
+        let mut inner = self.inner.write().await;
+        inner.start_vm(timeout).await
+    }
+
+    async fn stop_vm(&self) -> Result<()> {
+        let mut inner = self.inner.write().await;
+        inner.stop_vm().await
+    }
+
+    async fn pause_vm(&self) -> Result<()> {
+        let inner = self.inner.read().await;
+        inner.pause_vm()
+    }
+
+    async fn resume_vm(&self) -> Result<()> {
+        let inner = self.inner.read().await;
+        inner.resume_vm()
+    }
+
+    async fn save_vm(&self) -> Result<()> {
+        let inner = self.inner.read().await;
+        inner.save_vm().await
+    }
+
+    async fn add_device(&self, device: DeviceType) -> Result<()> {
+        let mut inner = self.inner.write().await;
+        inner.add_device(device).await
+    }
+
+    async fn remove_device(&self, device: DeviceType) -> Result<()> {
+        let mut inner = self.inner.write().await;
+        inner.remove_device(device).await
+    }
+
+    async fn get_agent_socket(&self) -> Result<String> {
+        let inner = self.inner.read().await;
+        inner.get_agent_socket().await
+    }
+
+    async fn disconnect(&self) {
+        let mut inner = self.inner.write().await;
+        inner.disconnect().await
+    }
+
+    async fn hypervisor_config(&self) -> HypervisorConfig {
+        let inner = self.inner.read().await;
+        inner.hypervisor_config()
+    }
+
+    async fn get_thread_ids(&self) -> Result<VcpuThreadIds> {
+        let inner = self.inner.read().await;
+        inner.get_thread_ids().await
+    }
+
+    async fn cleanup(&self) -> Result<()> {
+        let inner = self.inner.read().await;
+        inner.cleanup().await
+    }
+
+    async fn resize_vcpu(&self, old_vcpu: u32, new_vcpu: u32) -> Result<(u32, u32)> {
+        let inner = self.inner.read().await;
+        inner.resize_vcpu(old_vcpu, new_vcpu).await
+    }
+
+    async fn get_pids(&self) -> Result<Vec<u32>> {
+        let inner = self.inner.read().await;
+        inner.get_pids().await
+    }
+
+    async fn get_vmm_master_tid(&self) -> Result<u32> {
+        let inner = self.inner.read().await;
+        inner.get_vmm_master_tid().await
+    }
+
+    async fn get_ns_path(&self) -> Result<String> {
+        let inner = self.inner.read().await;
+        inner.get_ns_path().await
+    }
+
+    async fn check(&self) -> Result<()> {
+        let inner = self.inner.read().await;
+        inner.check().await
+    }
+
+    async fn get_jailer_root(&self) -> Result<String> {
+        let inner = self.inner.read().await;
+        inner.get_jailer_root().await
+    }
+
+    async fn save_state(&self) -> Result<HypervisorState> {
+        self.save().await
+    }
+    async fn capabilities(&self) -> Result<Capabilities> {
+        let inner = self.inner.read().await;
+        inner.capabilities().await
+    }
+}
+#[async_trait]
+impl Persist for Firecracker {
+    type State = HypervisorState;
+    type ConstructorArgs = ();
+    /// Save a state of the component.
+    async fn save(&self) -> Result<Self::State> {
+        let inner = self.inner.read().await;
+        inner.save().await.context("save hypervisor state")
+    }
+    /// Restore a component from a specified state.
+    async fn restore(
+        hypervisor_args: Self::ConstructorArgs,
+        hypervisor_state: Self::State,
+    ) -> Result<Self> {
+        let inner = FcInner::restore(hypervisor_args, hypervisor_state).await?;
+        Ok(Self {
+            inner: Arc::new(RwLock::new(inner)),
+        })
+    }
+}
